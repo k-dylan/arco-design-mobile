65c15a8f89649c3797caa3a6a57eccf6d9a99724
dylan <kdylan@qq.com>
2025-01-09 21:27:00 +0800

    feat: 修改npm仓库

--- scripts/publish/pre-publish.js
+++ scripts/publish/pre-publish.js
@@ -6,7 +6,11 @@ if (!fs.existsSync('.git')) {
     process.exit(1);
 }
 
-if (!~execSync('npm config get registry').toString().search('https://registry.npmjs.org/')) {
+if (
+    !~execSync('npm config get registry')
+        .toString()
+        .search('https://packages.aliyun.com/5ffd87467c4a6a0f9774e601/npm/npm-registry/')
+) {
     console.log('please check npm registry');
     process.exit(1);
-};
+}
